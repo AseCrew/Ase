fb97cb3ee3c5e47cdf34b9397f21f93a53e16cfe
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-25 06:12:11 -0300

    Implement state management in Ase class
    Added MenuState import and implemented state switching in Ase class.

--- source/Main.js
+++ source/Main.js
@@ -1,11 +1,23 @@
+import MenuState from "./ai/menus/MenuState.js";
+
 class Ase {
   constructor() {
     this.name = "Ase";
     this.version = "1.0.0";
+    this.currentState = null;
   }
 
   init() {
     console.log(`${this.name} initialized.`);
+    this.switchState(new MenuState());
+  }
+
+  switchState(newState) {
+    if (this.currentState) {
+      this.currentState.exit();
+    }
+    this.currentState = newState;
+    this.currentState.enter();
   }
 }
 
